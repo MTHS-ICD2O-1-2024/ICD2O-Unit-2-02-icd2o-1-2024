b862a99b1e96859e7dc41ba06d1476f084ee42f7
icd2o-1-2024 <icd2o-1-2024@mths.ca>
2025-03-07 17:56:36 +0000

    init

--- js/script.js
+++ js/script.js
@@ -8,5 +8,9 @@
  * Displays an alert with the message "Hello, World!"
  */
 function doMathClicked() {
-  alert("Hello, World!")
+  document.getElementById("add-math").innerHTML = "<p>6 + 5 = " + (6 + 5) + "</p>"
+  document.getElementById("subtract-math").innerHTML = "<p>7 - 3 = " + (7 - 3) + "</p>"
+  document.getElementById("multiply-math").innerHTML = "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>"
+  document.getElementById("divide-math").innerHTML = "(4 ÷ 2) + 3 = " + ((4 / 2) + 3) + "</p>"
+    document.getElementById("exponent-math").innerHTML = "5 + 2³ = " + (5 + 2**3) + "</p>"
 }
